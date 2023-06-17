84e14725307b45a066241cfd9118ddd1f0edcc74
Amokers <nicolas.bretecher@gmail.com>
2023-06-17 13:25:47 +0200

    v1

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 
 function ItemCard({ item, onBack }) {
   const effectParagraphs = item.effect.split('\n').map((paragraph, index) => (
-    <li key={index}>
-      <span className="item__effect-bullet">&#8226;</span> {paragraph}
-    </li>
+    <li key={index}>{paragraph}</li>
   ));
 
   return (
     <div className="item__card">
-      <img src={"src/img/" + item.id + ".webp"} alt="" className="item__pic" />
-      <h1 className="card__title">{item.title}</h1>
-      <p className="card__pickup">{item.pickup}</p>
-      <ul className="card__effect">
-        {effectParagraphs}
-      </ul>
-      <button className="back__button" onClick={onBack}>Back</button>
+        <div className='card__header'>
+            <img src={"src/img/" + item.id + ".webp"} alt="" className="item__pic" />
+            <h1 className="card__title">{item.title}</h1>
+            <p className="card__pickup">{item.pickup}</p>
+        </div>
+        <ul className="card__effect">
+            {effectParagraphs}
+        </ul>
+        <button className="back__button" onClick={onBack}>Back</button>
     </div>
   );
 }
